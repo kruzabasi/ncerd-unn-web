b47b419862c5de842e36cf0f9479bc7a47997cd2
kruzabasi <kruzabasi@gmail.com>
2021-07-01 10:56:39 +0100

    responsive navbar

--- src/components/nav/components/Nav.jsx
+++ src/components/nav/components/Nav.jsx
@@ -15,6 +15,10 @@ export default function Nav() {
       text: "Home"
     },
     {
+      link: "/news/",
+      text: "News & Events"
+    },
+    {
       link: "/projects/",
       text: "Projects"
     },
