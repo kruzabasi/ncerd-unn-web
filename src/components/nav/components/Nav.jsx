72e2b142d312f3301dca8e3376aef78ba22ea38f
kruzabasi <kruzabasi@gmail.com>
2021-07-01 10:44:34 +0100

    responsive navbar

---
+++ src/components/nav/components/Nav.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { FaAlignRight, FaTimes } from "react-icons/fa";
+import ncerdlogo from "../assets/ncerdlogo.png";
+import "./styles.css";
+
+export default function Nav() {
+  const [toggle, setToggle] = useState(false);
+  const Toggle = () => {
+    setToggle(!toggle);
+  };
+
+  const li = [
+    {
+      link: "/",
+      text: "Home"
+    },
+    {
+      link: "/projects/",
+      text: "Projects"
+    },
+    {
+      link: "/staff/",
+      text: "Staff Profiles"
+    },
+    {
+      link: "/about/",
+      text: "About"
+    },
+    {
+      link: "/contact/",
+      text: "Contact Us"
+    }
+  ];
+  return (
+    <div className="navBar">
+      <button onClick={Toggle}>
+        {toggle ? <FaTimes /> : <FaAlignRight />}
+      </button>
+      <div className="navItems">
+        <div>
+          <img src={ncerdlogo} alt="NCERD-Logo" className="ncerdlogo" />
+        </div>
+        <ul className={toggle ? "links show-nav" : "links"}>
+          {li.map((objLink, i) => {
+            return (
+              <li key={i}>
+                <a href={objLink.link}>{objLink.text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <hr className="nav-border" />
+    </div>
+  );
+}
