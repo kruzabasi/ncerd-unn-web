3bc4e0085bfc87a48cccfd01654843e1fe1d5ce1
kruzabasi <kruzabasi@gmail.com>
2021-07-06 13:31:02 +0100

    routing & styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/nav/components/Nav";
+import Home from "./components/home/components/Home";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
     </div>
   );
 }
